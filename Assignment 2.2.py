29476acb2101a29e614b4873c3a13e960c129c9d
shubhamyadav80002787-arch <shubhamyadav80002787@gmail.com>
2025-11-20 19:37:52 +0530

    Add files via upload

---
+++ Assignment 2.2.py
@@ -0,0 +1,4 @@
+sum = 0
+for number in range (1,51):
+    sum=sum +number
+print(f"The sum of numbers from 1 to 50 is:{sum}")
---
+++ Assingnment 2.py
@@ -0,0 +1,5 @@
+int(input("Enter a number :"))
+if 7%2 == 0:
+    print("12 is  an even number.")
+else:
+    print("7 is an odd number.")      
